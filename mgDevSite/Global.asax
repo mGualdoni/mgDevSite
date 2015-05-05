3bca4a0ccf750a1725b8585582ae1c310573b592
mgualdoni <matt.gualdoni@gmail.com>
2015-05-04 22:27:07 -0400

    Initial Commit

---
+++ mgDevSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mgDevSite.MvcApplication" Language="C#" %>
---
+++ mgDevSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mgDevSite.Startup))]
+namespace mgDevSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
